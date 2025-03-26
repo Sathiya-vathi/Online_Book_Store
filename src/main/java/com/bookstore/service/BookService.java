ba4e2a3c604e0c5eb6c6cf3c364e709fc6d45c1a
Sathiya-vathi <Sathiyavathi2003@gmail.com>
2025-03-26 12:36:14 +0530

    Initial commit - Added SpringBoot project

---
+++ src/main/java/com/bookstore/service/BookService.java
@@ -0,0 +1,55 @@
+package com.bookstore.service;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bookstore.dao.BookRepository;
+import com.bookstore.entities.Book;
+import com.bookstore.exception.ResourceNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Service
+public class BookService {
+	
+	@Autowired
+	private BookRepository bookRepository;
+	
+	private static final Logger logger = LoggerFactory.getLogger(BookService.class);
+	
+	// Create
+	public Book addBook(Book book) {
+		logger.info("Adding new book: {}", book);
+		return bookRepository.save(book);
+	}
+	
+	// Read all
+	public List<Book> getAllBooks() {
+		logger.info("Fetching all books");
+		return bookRepository.findAll();
+	}
+	
+	
+	
+	// Update
+	public Book updateBook(Long id, Book bookDetails) {
+		logger.info("Updating book with ID: {}", id);
+		Book book = getBookById(id);
+		book.setTitle(bookDetails.getTitle());
+		book.setAuthor(bookDetails.getAuthor());
+		book.setPrice(bookDetails.getPrice());
+		book.setPublishedDate(bookDetails.getPublishedDate());
+		Book updatedBook = bookRepository.save(book);
+		logger.info("Book updated successfully: {}", updatedBook);
+		return updatedBook;
+	}
+	
+	// Delete
+	public void deleteBook(Long id) {
+		logger.info("Deleting book with ID: {}", id);
+		Book book = getBookById(id);
+		bookRepository.delete(book);
+		logger.info("Book deleted successfully");
+	}
+}
